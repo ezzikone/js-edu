d8f8e0aec7a4ee627340da180015d1427a0f4b7b
ezzikone <47362010+ezzikone@users.noreply.github.com>
2019-10-01 16:59:15 +0400

    feat
    complete task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+module.exports = function getTimeForEducation(preferences, knowsProgramming, config) {
+
+    if (knowsProgramming) {                                            //условие знает ли ученик программирование
+        for (let key in config) {                                      //цикл перебора каждого key в config
+            if (preferences === key) {                                 //условие
+                return Math.ceil(800/config[key]);                  //возвращаем значение округлённое в большую сторону (т.к. неделя не может быть округлена в
+                                                                       //меньшую сторону) при каком приоритете ученик потратит 800 часов в неделю и выучит
+            }
+        }
+    }
+
+    else {
+        for (let key in config) {                                      //цикл перебора каждого key в config
+            if (preferences === key) {                                 //условие
+                return Math.ceil(1300/config[key]);                 //возвращаем значение округлённое в большую сторону (т.к. неделя не может быть округлена в
+                                                                       //меньшую сторону) при каком приоритете ученик потратит 800 часов в неделю и выучит
+            }
+        }
+    }
+
   };
   
